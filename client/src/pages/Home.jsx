57416b4989c0f6a6931849cf769ef5f8653c4dda
pradip2004 <pradipsadhukhan16@gmail.com>
2024-08-01 00:59:48 +0530

    done homepage frontend

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,21 @@
 import React, { useState } from 'react'
 import {Card, Loader, FormField} from '../components'
+
+const Rendercards = ({data, title}) => {
+  if(data?.length > 0) {
+    return data.map((post)=><Card key={post._id} {...post} />)
+    }
+
+    return (
+      <h2 className='mt-5 font-bold text-[#6449ff] text-xl uppercase'>{title}</h2>
+    )
+  }
+
+
 function Home() {
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(false)
+  const [allpost, setAllpost] = useState(null)
+  const [searchText, setSearchText] = useState('')
   return (
     <section className='max-w-7xl mx-auto'>
       <h1 className='font-extrabold text-[#222328] text-[32px]'>The Community Showcase</h1>
@@ -17,7 +31,21 @@ function Home() {
             <Loader />
           </div>
         ): (
-          <></>
+          <>
+            {searchText && (
+              <h2 className='font-medium text-[#666e75] text-xl mb-3'>
+                Showing results for <span className='text-[#222328]'>{searchText}</span>
+              </h2>
+            )}
+
+            <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
+              {searchText ? (
+                <Rendercards data={[]} title="No search results found" />
+              ) : (
+                <Rendercards data={[]} title = "No post found" />
+              )}
+            </div>
+          </>
         )}
       </div>
     </section>
